65dd643a1d02c7a21a465057cf43e6a3698ec923
Thomas Smith <72811986+3l5@users.noreply.github.com>
2020-10-22 19:27:50 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+var request = require('request')
+var token = "insertyourusertokenhere"
+var groupId = "groupchatid"
+setInterval(()=>{
+ var okretard = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+ request.patch({
+                headers: {'Authorization': token,'Content-Type':'application/json'},
+                url: `https://discordapp.com/api/v6/channels/${groupId}`,
+                body:`{"name":"${okretard}"}`
+                }, function (error, response, body) 
+               {
+          });
+}, 25)
+// credits : Zool
